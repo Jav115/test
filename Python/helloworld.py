26567ed290fc98aa1763cf1342cefcdc3f97b7d4
Javier Solis <40665826+Jav115@users.noreply.github.com>
2019-06-27 09:46:30 -0600

    Add files via upload

---
+++ Python/helloworld.py
@@ -0,0 +1,6 @@
+print("Hello, World!")
+import random
+
+print(random.randrange(1,100))
+b = "Hello, World!"
+print(b[2:5])
---
+++ Python/myfile.py
@@ -0,0 +1,6 @@
+if 5 > 2:
+    print("Five is greater than two!")
+X= 5
+y= "Hello, World!"
+#this is a coment.
+print("Hello, World!")
